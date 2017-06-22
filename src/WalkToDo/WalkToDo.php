72c00b746fc797810843bc823f5d6d84873bb03c
Thouv <al2reims@outlook.fr>
2017-06-22 17:39:32 -0400

    Add basic API methods

--- src/WalkToDo/WalkToDo.php
+++ src/WalkToDo/WalkToDo.php
@@ -6,45 +6,100 @@ namespace WalkToDo;
 
 use pocketmine\plugin\PluginBase;
 
+use pocketmine\level\Level;
+use pocketmine\level\Position;
+
+use WalkToDo\WalkToDoBlock;
+
 
 class WalkToDo extends PluginBase
 {
   
-  public $connection;
   public $cfg;
-  public $pingTask;
   public $blocks;
   
   
   public function onEnable()
   {
     
-    $this->cfg = $this->getConfig()->getAll();
+    $this->saveDefaultConfig();
+    $this->cfg = $this->getConfig();
     
-    $mysql_creds = $this->cfg['mysql']; // array of mysql credentials
-    $this->connection = new \mysqli( $mysql_creds['host'], $mysql_creds['username'], $mysql_creds['password'], $mysql_creds['database'], $mysql_creds['port'] );
-    
-    $this->getDatabase()->query
-    
-    $this->getServer()->getPluginManager()->registerEvents(new \WalkToDo\EventListener($this), $this);
-    $this->getServer()
+    $this->getServer()->getPluginManager()->registerEvents( new \WalkToDo\EventListener( $this ), $this );
+
+    // set command executors
+    $this->getCommand( "walktodo" )->setExecutor( new \Command\WalkToDoCommand( $this ), $this );
+
+    // get all the blocks in $this->blocks
+    $this->blocks = array();
+    $this->parseBlocks();
     
   }
+
   
-  
-  public function getDatabase()
+  public function getCfg()
   {
-    
-    if( $this->connection instanceof \mysqli )
+
+    return $this->cfg;
+
+  }
+
+
+  public function parseBlocks()
+  {
+
+    foreach( $this->cfg->get("blocks") as $block )
     {
-      return $this->connection;
+
+        $coords = $block["x"] . ":" . $block["y"] . ":" . $block["z"] . ":" . $block["level"];
+        $this->blocks[$coords] = new WalkToDoBlock( new Position( $block["x"], $block["y"], $block["z"], $this->getServer()->getLevelByName( $block["level"] ) ), $block["commands"], $this, count($this->blocks) ); // WalkToDoBlock class takes an instance of Position, an array of commands, an instance of WalkToDo, and an id
+
+    }
+
+  }
+
+
+  public function getBlock( $x, $y, $z, Level $level )
+  {
+
+    $coords = $x . ":" . $y . ":" . $z . ":", $level->getName();
+    return isset( $this->blocks[$coords] ) ? $this->blocks[$coords] : false; // returns the WalkToDo block if it exists, otherwise false
+
+  }
+
+
+  public function addBlock( Position $pos, array $commands )
+  {
+
+    $coords = $pos->getX() . ":" . $pos->getY() . ":" . $pos->getZ() . ":" . $pos->getLevel();
+    if( ! isset( $this->blocks[$coords] ) )
+    {
+
+        $this->blocks[$coords] = new WalkToDoBlock( $pos, $commands, $this, count( $this->blocks ) );
+        
+        $blocks = $this->cfg->get("blocks");
+        $blocks[$this->blocks[$coords]->getId()] = array(
+            "x" => $pos->getX();
+            "y" => $pos->getY();
+            "z" => $pos->getZ();
+            "level" => $pos->getLevel()->getName();
+            "commands" => $commands;
+        );
+        $this->cfg->set( "blocks", $blocks );
+        $this->cfg->save();
+
     }
     else
     {
-      return false;
+
+        return false;
+
     }
-    
+
   }
+
+
+  public function removeBlock(  )
   
 }
 
