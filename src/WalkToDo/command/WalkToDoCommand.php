196d51ffdd2445446fb3e41829b608a84dff6d23
Kryphum <kryphum@gmail.com>
2017-06-24 09:57:08 -0400

    Add WalkToDoCommand.php, add aliases to plugin.yml

---
+++ src/WalkToDo/command/WalkToDoCommand.php
@@ -0,0 +1,66 @@
+<?php
+
+
+namespace WalkToDo\command;
+
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\command\CommandExecutor;
+
+use pocketmine\utils\TextFormat;
+
+use WalkToDo\WalkToDo;
+
+
+class WalkToDoCommand implements CommandExecutor
+{
+
+    public $plugin;
+
+
+    public function __construct( WalkToDo $plugin )
+    {
+
+        $this->plugin = $plugin;
+
+    }
+
+
+    public function onCommand( Command $cmd, CommandSender $sender, $label, array $args )
+    {
+
+        if( $cmd->getName() == "walktodo" )
+        {
+
+            if( count( $args ) < 1 )
+            {
+
+                $sender->sendMessage( TextFormat::WHITE . "[WalkToDo] " . TextFormat::GOLD . "Usage: /walktodo <command> [| <command> [| <command>[...]]]");
+
+                return true;
+
+            }
+
+            // user can add multiple commands with /walktodo command1 | command2 | command3 | etc
+            foreach( $args as $key => $arg )
+            {
+
+                if( $arg === "|" )
+                {
+
+                    $this->plugin->sessions[$sender->getName()][] = implode( " ", array_slice( &$args, 0, $key + 1 ) );
+
+                }
+
+                $this->plugin->sessions[$sender->getName()] = implode( " ", array_slice( &$args, 0, count( $args ) ) );
+
+            }
+
+            $sender->sendMessage( TextFormat::WHITE . "[WalkToDo] " . TextFormat::GREEN . "Please tap a block to add the commands to!" );
+
+        }
+
+    }
+
+}
